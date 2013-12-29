0c82f5f57edb817ada6643b0c9cb3e09a28d0268
Victor Rocha <victor@rochapps.com>
2013-12-29 11:32:09 -0500

    Update urls.py
    Django 1.6

--- paypal/standard/pdt/urls.py
+++ paypal/standard/pdt/urls.py
@@ -1,4 +1,7 @@
-from django.conf.urls.defaults import *
+try:
+    from django.conf.urls import *
+except:
+    from django.conf.urls.defaults import *
 
 urlpatterns = patterns('paypal.standard.pdt.views',
     url(r'^$', 'pdt', name="paypal-pdt"),
