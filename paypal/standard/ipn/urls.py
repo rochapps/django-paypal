835a0f90ac5a5590c3c470ae6412f44c10bef280
Victor Rocha <victor@rochapps.com>
2013-12-29 11:31:02 -0500

    Update urls.py
    the defaults module was deprecated on Django 1.6. This adds Django 1.6 compatibility.

--- paypal/standard/ipn/urls.py
+++ paypal/standard/ipn/urls.py
@@ -1,4 +1,7 @@
-from django.conf.urls.defaults import *
+try:
+    from django.conf.urls import *
+except:
+    from django.conf.urls.defaults import *
 
 urlpatterns = patterns('paypal.standard.ipn.views',            
     url(r'^$', 'ipn', name="paypal-ipn"),
